15192b9472420a1dd3374a05be988ecf9f1c244a
Makiras <xieyunlonglong@gmail.com>
2021-05-22 16:25:41 +0800

    [sync] add registry ,translations & fix some bug/feature

--- frontend/js/main/register-users.js
+++ frontend/js/main/register-users.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 /* eslint-disable
     max-len,
     no-return-assign,
-*/
+    */
 // TODO: This file was created by bulk-decaffeinate.
 // Fix any style issues and re-enable lint.
 /*
@@ -14,37 +14,57 @@ import _ from 'lodash'
 import App from '../base'
 
 export default App.controller('RegisterUsersController', function(
-  $scope,
-  queuedHttp
+	$scope,
+	queuedHttp
 ) {
-  $scope.users = []
+	$scope.users = []
 
-  $scope.inputs = { emails: '' }
+	$scope.inputs = { emails: '' }
 
-  const parseEmails = function(emailsString) {
-    const regexBySpaceOrComma = /[\s,]+/
-    let emails = emailsString.split(regexBySpaceOrComma)
-    emails = _.map(emails, email => (email = email.trim()))
-    emails = _.filter(emails, email => email.indexOf('@') !== -1)
-    return emails
-  }
+	const parseEmails = function(emailsString) {
+		const regexBySpaceOrComma = /[\s,]+/
+		let emails = emailsString.split(regexBySpaceOrComma)
+		emails = _.map(emails, email => (email = email.trim()))
+		emails = _.filter(emails, email => email.indexOf('@') !== -1)
+		return emails
+	}
 
-  return ($scope.registerUsers = function() {
-    const emails = parseEmails($scope.inputs.emails)
-    $scope.error = false
-    return Array.from(emails).map(email =>
-      queuedHttp
-        .post('/admin/register', {
-          email,
-          _csrf: window.csrfToken
-        })
-        .then(function(response) {
-          const { data } = response
-          const user = data
-          $scope.users.push(user)
-          return ($scope.inputs.emails = '')
-        })
-        .catch(() => ($scope.error = true))
-    )
-  })
+	return (
+		$scope.registerUsers = function() {
+			const emails = parseEmails($scope.inputs.emails)
+			$scope.error = false
+			return Array.from(emails).map(email =>
+				queuedHttp
+				.post('/admin/register', {
+					email,
+					_csrf: window.csrfToken
+				})
+				.then(function(response) {
+					const { data } = response
+					const user = data
+					$scope.users.push(user)
+					return ($scope.inputs.emails = '')
+				})
+				.catch(() => ($scope.error = true))
+			)
+		},
+		$scope.registerUsersPub = function() {
+			const emails = parseEmails($scope.inputs.emails)
+			$scope.error = false
+			return Array.from(emails).map(email =>
+				queuedHttp
+				.post('/register', {
+					email,
+					_csrf: window.csrfToken
+				})
+				.then(function(response) {
+					const { data } = response
+					const user = data
+					$scope.users.push(user)
+					return ($scope.inputs.emails = '')
+				})
+				.catch(() => ($scope.error = true))
+			)
+		}
+	)
 })
